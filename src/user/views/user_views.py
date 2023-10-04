2a1761afd3a0c1139bc3e6efa570f7799b15a548
Manveer Basra <m13basra@gmail.com>
2023-10-03 19:54:05 -0700

    Include prediction market vote as a user contribution

--- src/user/views/user_views.py
+++ src/user/views/user_views.py
@@ -31,6 +31,7 @@ from paper.models import Paper
 from paper.serializers import DynamicPaperSerializer
 from paper.utils import PAPER_SCORE_Q_ANNOTATION, get_cache_key
 from paper.views import PaperViewSet
+from prediction_market.models import PredictionMarketVote
 from reputation.models import Bounty, BountySolution, Contribution, Distribution
 from reputation.serializers import (
     DynamicBountySerializer,
@@ -1244,6 +1245,8 @@ class AuthorViewSet(viewsets.ModelViewSet):
                     "title",
                     "user_flag",
                     "user_vote",
+                    "vote",
+                    "bet_amount",
                 ]
             },
             "rep_dbs_get_item": {
@@ -1440,6 +1443,9 @@ class AuthorViewSet(viewsets.ModelViewSet):
         review_content_type = ContentType.objects.get_for_model(Review)
         bounty_content_type = ContentType.objects.get_for_model(Bounty)
         bounty_solution_content_type = ContentType.objects.get_for_model(BountySolution)
+        replication_vote_content_type = ContentType.objects.get_for_model(
+            PredictionMarketVote
+        )
 
         types = asset_type.split(",")
 
@@ -1512,8 +1518,15 @@ class AuthorViewSet(viewsets.ModelViewSet):
                     content_type_id=bounty_solution_content_type,
                     contribution_type__in=[Contribution.BOUNTY_SOLUTION],
                 )
+            elif asset_type == "replication_vote":
+                query |= Q(
+                    unified_document__is_removed=False,
+                    user__author_profile=author_id,
+                    content_type_id=replication_vote_content_type,
+                    contribution_type__in=[Contribution.REPLICATION_VOTE],
+                )
             else:
-                raise Exception("Unrecognized asset type")
+                raise Exception("Unrecognized asset type: {}".format(asset_type))
 
         qs = (
             Contribution.objects.filter(query)
